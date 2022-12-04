f4abfb1022435eff452b62e186b32715261feb10
alehandrod alehandrod <alehandrod@yandex.ru>
2022-12-04 15:35:47 +0700

    Добавьте файлы проекта.

---
+++ Tree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add("привет", 50);
+            tree.Add("мир", 100);
+            tree.Add("приз", 200);
+            tree.Add("мирный", 50);
+            tree.Add("подарок", 100);
+            tree.Add("проект", 200);
+            tree.Add("прапорщик", 100);
+            tree.Add("правый", 200);
+            tree.Add("год", 50);
+            tree.Add("герой", 100);
+            tree.Add("красота", 300);
+            tree.Add("голубь", 200);
+            tree.Add("прокрастинация", 1000);
+
+            tree.Remove("правый");
+            tree.Remove("мир");
+
+            Search(tree, "привет");
+            Search(tree, "мир");
+            Search(tree, "облако");
+            Search(tree, "мирный");
+            Search(tree, "прокрастинация");
+            Search(tree, "год");
+
+            Console.ReadLine();
+        }
+
+        private static void Search(Tree<int> tree, string word)
+        {
+            if (tree.TrySearch(word, out int value))
+            {
+                Console.WriteLine(word + " " + value);
+            }
+            else
+            {
+                Console.WriteLine("Не найдено " + word);
+            }
+        }
+    }
+}
---
+++ Tree/Tree.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Tree
+{
+    public class Tree<T>
+    {
+        private Node<T> _root;
+
+        public Tree()
+        {
+            _root = new Node<T>('\0', default, "");
+            Count = 1;
+        }
+
+        public int Count { get; set; }
+
+        public void Add(string key, T data)
+        {
+            AddNode(key, data, _root);
+        }
+
+        private void AddNode(string key, T data, Node<T> node)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                if (!node.IsWord)
+                {
+                    node.Data = data;
+                    node.IsWord = true;
+                }
+            }
+            else
+            {
+                var subnode = node.TryFind(key[0]);
+                if (subnode != null)
+                {
+                    AddNode(key.Substring(1), data, subnode);
+                }
+                else
+                {
+                    var newNode = new Node<T>(key[0], data, node.Prefix + key[0]);
+                    node.SubNodes.Add(key[0], newNode);
+                    AddNode(key.Substring(1), data, newNode);
+                }
+            }          
+        }
+
+        public void Remove(string key)
+        {
+            RemoveNode(key, _root);
+        }
+
+        private void RemoveNode(string key, Node<T> node)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                if (node.IsWord)
+                {
+                    node.IsWord = false;
+                }
+            }
+            else
+            {
+                var subnode = node.TryFind(key[0]);
+                if (subnode != null)
+                {
+                    RemoveNode(key.Substring(1), subnode);
+                }
+            }
+        }
+
+        public bool TrySearch(string key, out T value)
+        {
+            return SearchNode(key, _root, out value);
+        }
+
+        private bool SearchNode(string key, Node<T> node, out T value)
+        {
+            value = default;
+            var result = false;
+
+            if (string.IsNullOrEmpty(key))
+            {
+                if (node.IsWord)
+                {
+                    value = node.Data;
+                    result = true;
+                }
+            }
+            else
+            {
+                var subnode = node.TryFind(key[0]);
+                if (subnode != null)
+                {
+                    result = SearchNode(key.Substring(1), subnode, out value);
+                }
+            }
+
+            return result;
+        }
+    }
+}
